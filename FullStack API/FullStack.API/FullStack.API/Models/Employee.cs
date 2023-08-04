eb57bd00ffdc0c9d151afc4eef8ed94c43f4104b
elyonousimohamed <melyonousi@redimpactarabia.com>
2023-08-04 10:32:34 +0100

    initial

---
+++ FullStack API/FullStack.API/FullStack.API/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace FullStack.API.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Profile { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public long Phone { get; set; }
+        public long Salary { get; set; }
+        public string Department { get; set; }
+    }
+}
